5dd49940e7f29763d5b75294500a051a692b6ad6
ws0671 <ws0671@naver.com>
2021-01-29 11:24:59 +0900

    feat: Recreate print function

--- hello.py
+++ hello.py
@@ -1,2 +1,6 @@
-for _ in range(5):
-    print('hello, git!')
+def print_hello():
+    for i in range(1,10):
+        print('hello, git for {}th time(s)!'.format(i))
+
+if __name__=='__main__':
+    print_hello()
